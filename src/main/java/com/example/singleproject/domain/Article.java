0b407f6f23e58c9d4745483f6827a5ce4e03bf88
Bae Ji Won <qowhxk@naver.com>
2023-11-14 23:58:48 +0900

    #5 Article Entity DB와 맵핑 1. Colum 설정 2. Auditing 설정 3. equals & hashcode 동등성 검사

--- src/main/java/com/example/singleproject/domain/Article.java
+++ src/main/java/com/example/singleproject/domain/Article.java
@@ -1,14 +1,79 @@
 package com.example.singleproject.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;   // 제목
+    @Setter
+    @Column(nullable = false)
+    private String title;   // 제목       Setter를 개별 변수에 걸어 외부에서 id에는 접근하지 못하도록 함
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;     // 내용
-    private String hashtag;     // 해시태그
+    @Setter private String hashtag;     // 해시태그
+
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;    // 생성일자
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;       // 생성자
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;       // 수정일자
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;          // 수정자
+
+    protected Article() {}      // 생성자1
+
+    private Article(String title, String content, String hashtag) {     // 생성자2
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title,content,hashtag);
+    }
+
+    
+    /* equals && hashCode는 현재 Entity와 DB에 저장된 테이블이 일치하는지 매칭검사하는 용으로 여러개의 컬럼들 중
+       유니크 값을 가지는 id만 비교해도 됨(만약 DB에서 content내용을 변경한 경우는 찾을수가 없기때문에 그때는 Lombok의 @EqualsAndHashCode어노테이션을 클래스에 추가해줘서 모든 컬럼에 대해 비교시킴)
+     */
+    @Override
+    // 동등성 검사(영속화 검사 DB와 Entity가 1:1 매핑이 잘 되어 있는지 확인)
+    public boolean equals(Object o) {                                   // 객체를 입력받음
+        if (this == o) return true;                                     // 객체의 참조가 같은지 비교
+        if (o == null || getClass() != o.getClass()) return false;      // o가 null이거나 o의 클래스가 현재 클래스랑 다르다면 false 반환
+        Article article = (Article) o;                                  // 위의 경우가 아니라면 o객체를 Article객체로 캐스팅함
+        return id != null && id.equals(article.id);                     // 현재 id값이 무조건 있는 상태에서 현재 id와 방금전 캐스팅한 객체의 id값을 비교햐서 맞으면 true, 틀리면 false 반환
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
