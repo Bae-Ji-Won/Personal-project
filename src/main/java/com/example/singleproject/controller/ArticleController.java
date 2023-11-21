a3a066c469f57cc82999eea3107ff520c6005923
Bae Ji Won <qowhxk@naver.com>
2023-11-21 22:28:19 +0900

    #7 - ArticleController & Test Update

--- src/main/java/com/example/singleproject/controller/ArticleController.java
+++ src/main/java/com/example/singleproject/controller/ArticleController.java
@@ -1,6 +1,8 @@
 package com.example.singleproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/articles")
@@ -8,4 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class ArticleController {
 
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles","123");
+        return "articles/index";
+    }
 }
