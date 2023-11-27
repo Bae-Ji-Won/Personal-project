8dbc3729f7fa5b80221747c25e7ad01496db260d
Bae Ji Won <qowhxk@naver.com>
2023-11-27 19:00:31 +0900

    #7 - ArticleController View 이동 추가
    상세 페이지 View 이동

--- src/main/java/com/example/singleproject/controller/ArticleController.java
+++ src/main/java/com/example/singleproject/controller/ArticleController.java
@@ -3,7 +3,10 @@ package com.example.singleproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/articles")
 @Controller
@@ -15,4 +18,13 @@ public class ArticleController {
         map.addAttribute("articles","123");
         return "articles/index";
     }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId,ModelMap map){
+        map.addAttribute("article","article");
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
+    }
+
 }
