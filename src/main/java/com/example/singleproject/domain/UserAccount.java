d55d5e2c0d0d40e4d73b29c472e8e3d1165763f1
Bae Ji Won <qowhxk@naver.com>
2023-12-21 14:50:54 +0900

    기본키값 변경
    기존 기본키값인 ID(Long)에서 user_id(String)으로 변경

--- src/main/java/com/example/singleproject/domain/UserAccount.java
+++ src/main/java/com/example/singleproject/domain/UserAccount.java
@@ -17,12 +17,9 @@ import java.util.Objects;
 })
 @Entity
 public class UserAccount extends AuditingFields {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Setter
-    @Column(nullable = false, length = 50)
+    @Id
+    @Column(length = 50)
     private String userId;
     @Setter
     @Column(nullable = false)
@@ -55,13 +52,13 @@ public class UserAccount extends AuditingFields {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof UserAccount userAccount)) return false;
-        return id != null && id.equals(userAccount.id);
+        if (!(o instanceof UserAccount that)) return false;
+        return this.getUserId() != null && this.getUserId().equals(that.getUserId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(this.getUserId());
     }
 
 }
